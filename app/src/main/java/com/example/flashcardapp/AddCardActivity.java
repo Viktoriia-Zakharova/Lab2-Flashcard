a93b5b7c6fc9b69901749ef2e3c60a09c1377bed
Viktoriia-Zakharova <vzakhar1@swarthmore.edu>
2022-03-21 00:25:11 -0400

    Added Everything

---
+++ app/src/main/java/com/example/flashcardapp/AddCardActivity.java
@@ -0,0 +1,38 @@
+package com.example.flashcardapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+public class AddCardActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_card);
+
+        findViewById(R.id.cancel_button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+
+        findViewById(R.id.save_button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String question = ((EditText)findViewById(R.id.question_input)).getText().toString();
+                String answer = ((EditText) findViewById(R.id.answer_input)).getText().toString();
+                Intent data = new Intent(); // create a new Intent, this is where we will put our data
+                data.putExtra("string1", question); // puts one string into the Intent, with the key as 'string1'
+                data.putExtra("string2", answer); // puts another string into the Intent, with the key as 'string2
+                setResult(RESULT_OK, data); // set result code and bundle data for response
+                finish(); // closes this activity and pass data to the original activity that launched this activity
+            }
+        });
+
+    }
+}
---
+++ app/src/main/java/com/example/flashcardapp/MainActivity.java
@@ -0,0 +1,51 @@
+package com.example.flashcardapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        TextView flashcardQuestion = findViewById(R.id.flashcard_question);
+        TextView flashcardAnswer = findViewById(R.id.flashcard_answer);
+        flashcardQuestion.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                flashcardQuestion.setVisibility(View.INVISIBLE);
+                flashcardAnswer.setVisibility(View.VISIBLE);
+            }
+        });
+
+        findViewById(R.id.add_card_button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, AddCardActivity.class);
+                MainActivity.this.startActivityForResult(intent, 100);
+            }
+        });
+
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == 100) { // this 100 needs to match the 100 we used when we called startActivityForResult!
+            if (resultCode == RESULT_OK) {
+                String new_question = data.getExtras().getString("string1"); // 'string1' needs to match the key we used when we put the string in the Intent
+                String new_answer = data.getExtras().getString("string2");
+                ((TextView) findViewById(R.id.flashcard_question)).setText(new_question);
+                ((TextView) findViewById(R.id.flashcard_answer)).setText(new_answer);
+
+            }
+        }
+
+
+    }}
